41603f7d4f77bea48bfaba56ea749fb7c7d31901
Daulen Zhangabylov <daulenz@gmail.com>
2021-02-05 17:38:19 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -145,5 +145,6 @@ def adding_to_dennis
         :languages => ["C"]
       }
     }
-programmer_hash[:dennis_ritchie][:languages]=["C",]
+programmer_hash[:dennis_ritchie][:languages]=["C","Assembly" ]
+
 end
