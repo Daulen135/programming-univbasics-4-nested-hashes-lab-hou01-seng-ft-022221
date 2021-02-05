0efd38d253b7352dcf4a58ac1e67253e946e0137
Daulen Zhangabylov <daulenz@gmail.com>
2021-02-05 17:13:22 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -65,6 +65,8 @@ def dennis_ritchies_language
 programmer_hash[:dennis_ritchie][:languages]="C" #return as a string, otherwise ["c"] returns value of array
 end
 
+
+
 def adding_matz
 # add the following information to the top level of programmer_hash
 # :yukihiro_matsumoto => {
@@ -96,6 +98,8 @@ yukihiro_matsumoto =>
     }
 
 end
+
+
 
 def changing_alan
   # change what Alan Kay is :known_for to the value of the alans_new_info variable. 
